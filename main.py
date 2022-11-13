3c5c1a75b19f9da06300efebfad8c0e69cbd02da
Fabioyagui <fabioyagui@gmail.com>
2022-11-13 17:05:12 +0900

    Second Vers. height in cm.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
-height = float(input("enter your height in m: "))
+height = float(input("enter your height in cm: "))
 weight = int(input("enter your weight in kg: "))
-bmi = weight / height ** 2
+bmi = weight / (height/100) ** 2
 bmi_as_int = int(bmi)
 
 
