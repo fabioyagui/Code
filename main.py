0fcc9a5c0cd52bfe2562313d0b461949a8e25d8c
Fabioyagui <fabioyagui@gmail.com>
2022-11-13 16:59:54 +0900

    First Vers.

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+height = float(input("enter your height in m: "))
+weight = int(input("enter your weight in kg: "))
+bmi = weight / height ** 2
+bmi_as_int = int(bmi)
+
+
+
+print(bmi_as_int)
